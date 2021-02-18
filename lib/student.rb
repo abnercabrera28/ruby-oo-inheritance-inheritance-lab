6e0cf407c30caa0673cddb7e5a0003a661ca4c8a
Abner <abnercabrera28@gmail.com>
2021-02-18 17:59:01 -0500

    commit tree 2ac885029d6c7925dd067407fdf61fa75a79af20

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(words)
+        @knowledge << words
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
